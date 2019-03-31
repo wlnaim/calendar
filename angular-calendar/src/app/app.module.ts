6c8d476be391b9f0c8eb4249eef94cde02d0411d
Ernesto de la Cruz <ernesto.delacruz.morales@gmail.com>
2019-03-31 08:01:24 +0000

    setting up ng Module

--- angular-calendar/src/app/app.module.ts
+++ angular-calendar/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'modules/calendar.module';
+import {DateAdapter } from 'date-adapters/date-adapter';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })    
   ],
   providers: [],
   bootstrap: [AppComponent]
