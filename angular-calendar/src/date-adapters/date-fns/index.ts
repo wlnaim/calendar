e0f3d84b74a1f3197cf93edaa7c90ae36848da5b
Ernesto de la Cruz <ernesto.delacruz.morales@gmail.com>
2019-04-02 16:09:37 -0500

    Fixed broken dependencies and imports

--- angular-calendar/src/date-adapters/date-fns/index.ts
+++ angular-calendar/src/date-adapters/date-fns/index.ts
@@ -1,15 +1,15 @@
 import { adapterFactory as baseAdapterFactory } from 'calendar-utils/date-adapters/date-fns';
-import * as addWeeks from 'date-fns/add_weeks/index';
-import * as addMonths from 'date-fns/add_months/index';
-import * as subDays from 'date-fns/sub_days/index';
-import * as subWeeks from 'date-fns/sub_weeks/index';
-import * as subMonths from 'date-fns/sub_months/index';
-import * as getISOWeek from 'date-fns/get_iso_week/index';
-import * as setDate from 'date-fns/set_date/index';
-import * as setMonth from 'date-fns/set_month/index';
-import * as setYear from 'date-fns/set_year/index';
-import * as getDate from 'date-fns/get_date/index';
-import * as getYear from 'date-fns/get_year/index';
+import * as addWeeks from 'date-fns/add_weeks';
+import * as addMonths from 'date-fns/add_months';
+import * as subDays from 'date-fns/sub_days';
+import * as subWeeks from 'date-fns/sub_weeks';
+import * as subMonths from 'date-fns/sub_months';
+import * as getISOWeek from 'date-fns/get_iso_week';
+import * as setDate from 'date-fns/set_date';
+import * as setMonth from 'date-fns/set_month';
+import * as setYear from 'date-fns/set_year';
+import * as getDate from 'date-fns/get_date';
+import * as getYear from 'date-fns/get_year';
 import { DateAdapter } from '../date-adapter';
 
 export function adapterFactory(): DateAdapter {
